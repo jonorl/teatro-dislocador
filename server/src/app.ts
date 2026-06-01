2ab67a8642f5fa476d269598c4854c10c2a6921f
Jonathan Orlowski <39122230+jonorl@users.noreply.github.com>
2026-06-01 14:35:06 +0300

    Fix: Old route path

--- server/src/app.ts
+++ server/src/app.ts
@@ -55,7 +55,7 @@ app.use(
 app.use("/api/classes", classesRouter);
 app.use("/api/gallery", galleryRouter);
 app.use("/api/showcase", showcaseRouter);
-app.use("/api/upload", uploadRouter);
+app.use("/api/gallery/upload", uploadRouter);
 
 const PORT: number = Number(process.env.PORT_DISLOCADOR) || 3000;
 app.listen(PORT, "0.0.0.0", () => {
